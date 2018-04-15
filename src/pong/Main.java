7e4e64807a2fc534e6af667b55bbde3ed5b30238
BACHARDT <tom.bachard@ens-rennes.fr>
2018-04-16 01:34:20 +0200

    Level 2 added. Ball is sometimes stuck in the walls...

--- src/pong/Main.java
+++ src/pong/Main.java
@@ -13,10 +13,10 @@ public class Main extends Application
         Engine engine = new Engine(stage, width, height, 20, 200, 10, 19);
         World world = new World(engine);
 
-        Ball ball = new Ball(engine, world, new Vector2D(1, 1), new Vector2D(500, 250));
+        Ball ball = new Ball(engine, world, new Vector2D(1, 1), new Vector2D(width / 2, height / 2));
         new CircleBallRender(engine, ball);
 
-        world = Level.createLevel(world, engine,1);
+        world = Level.createLevel(world, engine,2);
 
         new AIRacket(engine, ball, RacketSide.LEFT, 0.2);
         new HumanRacket(engine, ball, RacketSide.RIGHT, 2);
